a2b0c01a111f35f4092c27ed4ac1fcd6a6665655
TasniaKheya <23kheya@gmail.com>
2024-06-03 19:22:54 +0600

    Student Class and List

---
+++ Student/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Class1
+    {
+        public int id;
+        public string firstName, middleName, lastName, dept;
+
+        public Class1(int id, string firstName, string middleName, string lastName, string dept)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.middleName = middleName;
+            this.lastName = lastName;
+            this.dept = dept;
+        }
+        public Class1() { }
+    }
+}
